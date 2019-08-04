2a3e8e1b29ef88b6486bfbd7ec1e36ddc7b5de45
isaac_manubag <isaac_manubag@yahoo.com>
2019-08-05 00:23:56 +0800

    added new screens

--- src/components/Secured/ProfileScreen/index.js
+++ src/components/Secured/ProfileScreen/index.js
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
-import moment from 'moment';
 import { logout } from '../../../redux/actions/auth';
 import { setActiveFast } from '../../../redux/actions/fasts';
-import * as Progress from 'react-native-progress';
 import styles from './styles';
 
 class ProfileScree extends React.Component {
